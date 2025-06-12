2a450f4e43ec07948bc40799f7a71b70441f5f60
ALPO <pumperer@gmail.com>
2025-06-12 21:20:35 +0900

    huditem base, controller 를 이곳으로 옮김

--- Runtime/Hud/HudItemPosition.cs
+++ Runtime/Hud/HudItemPosition.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace alpoLib.UI
+namespace alpoLib.UI.Hud
 {
     // [Flags]
     public enum HudItemPosition
